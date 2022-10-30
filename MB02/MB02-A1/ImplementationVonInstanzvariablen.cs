480a3af399f1ca926c6c48c44ac1fdff343fa399
damblatt <dam.blatt@gmail.com>
2022-10-30 13:38:14 +0100

    commit

---
+++ MB02/MB02-A1/ImplementationVonInstanzvariablen.cs
@@ -0,0 +1,10 @@
+﻿namespace MB02_A1
+{
+    internal class ImplementationVonInstanzvariablen
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
