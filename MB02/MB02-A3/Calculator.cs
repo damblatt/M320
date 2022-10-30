143effff8d0d7985b388fa8a91d162faa9bc0438
damblatt <dam.blatt@gmail.com>
2022-10-30 14:02:29 +0100

    commit

---
+++ MB02/MB02-A3/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB02_A3
+{
+    public class Calculator
+    {
+        private int TempResult { get; set; } = 0;       // interne speicherung?
+
+        public int Addition(int firstParam, int secondParam)
+        {
+
+            TempResult = firstParam + secondParam;
+            return TempResult;
+        }
+
+        public int Addition(int secondParam)
+        {
+            TempResult += secondParam;
+            return TempResult;
+        }
+
+        public int Subtraction(int firstParam, int secondParam)
+        {
+            TempResult = firstParam - secondParam;
+            return TempResult;
+        }
+
+        public int Subtraction(int secondParam)
+        {
+            TempResult -= secondParam;
+            return TempResult;
+        }
+
+        public int Multiplication(int firstParam, int secondParam)
+        {
+            TempResult = firstParam * secondParam;
+            return TempResult;
+        }
+
+        public int Multiplication(int secondParam)
+        {
+            TempResult *= secondParam;
+            return TempResult;
+        }
+
+        public int Division(int firstParam, int secondParam)
+        {
+            TempResult = firstParam / secondParam;
+            return TempResult;
+        }
+
+        public int Division(int secondParam)
+        {
+            TempResult /= secondParam;
+            return TempResult;
+        }
+    }
+}
---
+++ MB02/MB02-A3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MB02_A3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            int result;
+            string firstNumberVar;
+            int firstNumber;
+            string secondNumberVar;
+            int secondNumber;
+
+            Console.Write("first number: ");
+            firstNumberVar = Console.ReadLine();
+            firstNumber = Convert.ToInt32(firstNumberVar);
+            Console.Write("second number: ");
+            secondNumberVar = Console.ReadLine();
+            secondNumber = Convert.ToInt32(secondNumberVar);
+
+            result = calculator.Addition(firstNumber, secondNumber);
+            Console.WriteLine(result);
+            result = calculator.Addition(5);
+            Console.WriteLine(result);
+        }
+    }
+}
