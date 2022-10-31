6535accd370cbee90ce504cf0696f5539b0e0e8f
damblatt <dam.blatt@gmail.com>
2022-10-31 09:50:18 +0100

    commit

---
+++ MB03/MB03-2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace MB03_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rectangle1 = new Rectangle(5, 10);
+            var rectangle2 = new Rectangle(5);
+            var rectangle3 = new Rectangle(5, 10, "red");
+            var rectangle4 = new Rectangle(5, 10, 256);
+
+            var rectangle5 = new Rectangle(3)
+            {
+                Color = "green",
+                Alpha = 128
+            };
+        }
+    }
+}
---
+++ MB03/MB03-2/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB03_2
+{
+    internal class Rectangle
+    {
+        public int Height { get; set; } = 100;
+        public int Width { get; set; } = 200;
+        public string Color { get; set; } = "black";
+        public int Alpha { get; set; } = 1;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public Rectangle(int size) : this(size, size)
+        {
+
+        }
+
+        public Rectangle(int height, int width, string color) : this(height, width)
+        {
+            Color = color;
+        }
+
+        public Rectangle(int height, int width, int alpha) : this(height, width)
+        {
+            Alpha = alpha;
+        }
+    }
+}
