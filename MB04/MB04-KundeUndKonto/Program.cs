6753e896cc4926a1b80cbffe58593119d2a0657c
damblatt <dam.blatt@gmail.com>
2022-11-14 11:36:12 +0100

    commit

---
+++ MB04/MB04-KundeUndKonto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MB04_KundeUndKonto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var kunde = new Kunde();
+            var konto1 = new Konto(kunde);
+            var konto2 = new Konto(kunde);
+        }
+    }
+
+    public class Kunde
+    {
+        public List<Konto> Konten = new List<Konto>();
+
+        public void AddKonto(Konto konto)
+        {
+            if (Konten.Count < 5)
+            {
+                this.Konten.Add(konto);
+            } else
+            {
+                Console.WriteLine("Kunde kann nicht mehr Konten haben.");
+            }
+        }
+    }
+
+    public class Konto
+    {
+        public Konto(Kunde kunde)
+        {
+            kunde.AddKonto(this);
+        }
+    }
+}
