2b6fa20bd07a3efb01ccc737506f5565437f5962
damblatt <dam.blatt@gmail.com>
2022-11-26 23:01:50 +0100

    commit

---
+++ MB04/MB04-Vererbung/Kunde.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB04_Vererbung
+{
+    public class Kunde : Person
+    {
+        public int Umsatz { get; set; }
+
+        public Kunde(int identNr) : base(identNr)
+        {
+
+        }
+
+        public void Kaufe()
+        {
+
+        }
+
+        public override void Trage()
+        {
+        }
+    }
+}
---
+++ MB04/MB04-Vererbung/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MB04_Vererbung
+{
+    public abstract class Person
+    {
+        private int _key;
+
+        public string Name { get; set; }
+        public string Vorname { get; set; }
+
+        protected readonly int identNr;
+
+        public Person(int identNr)
+        {
+            this.identNr = identNr;
+        }
+
+        public virtual void Esse()
+        {
+
+        }
+
+        public abstract void Trage();
+    }
+}
